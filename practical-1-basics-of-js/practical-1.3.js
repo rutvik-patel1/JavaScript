4ee38e8a8028ff109348fd680792ba19900e3676
ruutvikpatel <rutvikpatel90990@gmail.com>
2022-02-08 15:38:16 +0530

    bug-fix

--- practical-1-basics-of-js/practical-1.3.js
+++ practical-1-basics-of-js/practical-1.3.js
@@ -1,6 +1,6 @@
 /**Write a function which takes two arrays of numbers as arguments and returns an
 array containing numbers either in the first or in the second array but not in both
-arrays. (use findIndex, forEach/for...of) */
+arrays. (use findIndex, forEach/for..of)*/
 
 let array1 = [1,2,3,4,5]
 let array2 = [4,5,6,7,8]
