55e65c632f0e8726ad74c2f17ecf1c16643090e7
ruutvikpatel <rutvikpatel90990@gmail.com>
2022-02-15 17:18:19 +0530

    add practical-3.4(incomplete)

--- practical-3-(array,string, math, object etc.)/practical-3.4.js
+++ practical-3-(array,string, math, object etc.)/practical-3.4.js
@@ -1,4 +1,3 @@
-/**Write a function which takes 2 objects as an argument and swaps each
-other without taking the temporary third object. Also return the array
-containing swapped objects and to hold those returned objects use the
-same objects you have passed to the function. (use ES6 concepts.) */
+/**Write a function which takes 2 objects as an argument and swaps each other without taking the
+temporary third object. Also return the array containing swapped objects and to hold those returned
+objects use the same objects you have passed to the function. (use ES6 concepts.) */
