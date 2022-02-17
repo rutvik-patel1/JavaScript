9a5ce34214f5aa4f612548274c4cfbb2dabdc653
ruutvikpatel <rutvikpatel90990@gmail.com>
2022-02-17 11:16:29 +0530

    add practical - 3.9

--- practical-3-(array,string, math, object etc.)/practical-3.9.js
+++ practical-3-(array,string, math, object etc.)/practical-3.9.js
@@ -1,5 +1,5 @@
 /**Write a function which takes an array containing fullNames of trainees
-and a string representing fullName as arguments. The function checks if
+and a str representing fullName as arguments. The function checks if
 the fullName is valid as well as unique means should not be present in the
 array. If so it adds the fullName to the array, else will skip adding it and
 returns relevant error messages.
@@ -16,6 +16,27 @@ for(let obj in jsonData) {
     fullNames.push(jsonData[obj].fullName)
 }
 
-const result = (string,array) => {
-    requiredArray = []
+const result = (str,array) => {
+    if(array.includes(str)) {
+        return console.log("same name already present !!")
+    }
+
+    let newStr = str.replace(/^\s+|\s+$/g, "").replace(/\s+/g, " ");
+    let condition = new RegExp("^[a-zA-Z ]*[']{0,1}[a-zA-Z ]*$");
+    if(str.match(/\d+/g) != null){
+        return console.log("Name contains number !!!");
+    }
+    else if(newStr != str){
+        return console.log("Name contains whitespace characters !!");
+    }
+    else if(!condition.test(str)){
+        return console.log("Apostrophes occururing multiple times !!");
+    }
+    else{
+        array.push(str);
+    }
+    return console.log(array); ;
 }
+
+
+result("DerekO'' Brien",fullNames)
